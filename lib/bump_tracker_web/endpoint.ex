24230dedeb7d3a2dfcbb400b467981cded416493
Jakub Hajto <hajtosek@gmail.com>
2017-10-28 17:29:03 +0200

    123

--- lib/bump_tracker_web/endpoint.ex
+++ lib/bump_tracker_web/endpoint.ex
@@ -47,6 +47,7 @@ defmodule BumpTrackerWeb.Endpoint do
   def init(_key, config) do
     if config[:load_from_system_env] do
       port = System.get_env("PORT") || raise "expected the PORT environment variable to be set"
+      IO.inspect(port, label: "Port read from configuration")
       {:ok, Keyword.put(config, :http, [:inet6, port: port])}
     else
       {:ok, config}
