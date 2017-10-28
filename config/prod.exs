c7b65db81f87b4129db1976dd760348c8077dc1c
Jakub Hajto <hajtosek@gmail.com>
2017-10-28 17:45:47 +0200

    123

--- config/prod.exs
+++ config/prod.exs
@@ -63,6 +63,3 @@ config :logger, level: :info
 #     config :bump_tracker, BumpTrackerWeb.Endpoint, server: true
 #
 
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
